d82749874fadcedaa8bd16b2bbb626d0dd8e7cf3
Scout7525 <scout7525@gmail.com>
2025-04-19 20:37:37 -0400

    Corrected _inference in main.py

--- main.py
+++ main.py
@@ -72,5 +72,5 @@ async def post_inference(data: Data):
         training=False,
         encoder=encoder,
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = None inference(model, data_processed)
     return {"result": apply_label(_inference)}
